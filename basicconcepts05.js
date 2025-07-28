ca83c69bfb97c9ef6e26b201577a04b3244725da
umar farooq <umarfarooq.workk@gmail.com>
2025-07-28 23:53:01 +0500

    Condition rendering Concept

---
+++ basicconcepts05.js
@@ -0,0 +1,22 @@
+// function App() {
+//   const isLoggedIn = true;
+
+//   if (isLoggedIn) {
+//     return <h1>Welcome, Aiman!</h1>;
+//   } else {
+//     return <h1>Please log in</h1>;
+//   }
+// }
+
+
+
+
+const isDarkMode = false;
+
+console.log(isDarkMode ? "Dark Mode Enabled" : "Light Mode Enabled");
+
+const showMessage = true;
+
+showMessage && console.log("This is a conditional message!");
+
+
